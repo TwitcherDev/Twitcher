e49467b0f8b9f0d1a36f172b6ccdb4b59b12a2db
LiphiTC <iprogcen@hotmail.com>
2022-03-08 17:37:41 +0300

    init commit

---
+++ src/Twitcher/Interfaces/IMessageReceiverArgs.cs
@@ -0,0 +1,9 @@
+﻿namespace Twitcher.Interfaces
+{
+    public interface IMessageReceiverArgs
+    {
+        public string Channel { get; }
+        public string UserName { get; }
+        public string Message { get; }
+    }
+}

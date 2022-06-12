8bfb7dafa13ea4892036deda8f6a247246e63986
Dan Dan <Dan94g@mail.ru>
2022-06-13 03:32:43 +0500

    move

--- src/Twitcher/Interfaces/IMessageReceiverArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Twitcher.Interfaces
-{
-    public interface IMessageReceiverArgs
-    {
-        public string Channel { get; }
-        public string UserName { get; }
-        public string Message { get; }
-    }
-}
